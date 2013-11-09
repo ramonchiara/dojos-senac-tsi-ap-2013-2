9fe19ff5bfbbefc21b60c473fde7238d60e4f518
Ramon Chiara <ramonchiara@gmail.com>
2013-11-09 11:41:06 -0200

    Adicionados: mapa mental sobre Coding Dojo, código e fotos

---
+++ Dojo1/VS.Net/Dojo1/Dojo1/Simulacao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo1
+{
+    class Simulacao
+    {
+        public static int ContarAeroportos(char[,] Mapa)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < Mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < Mapa.GetLength(1); j++)
+                {
+                    if (Mapa[i, j] == 'A')
+                    {
+                        cont += 1;
+                    }
+                }
+            }
+
+            return cont;
+        }
+
+        public static char[,] Expandir(char[,] Mapa)
+        {
+            char[,] esperado = new char[3, 3];
+
+            for (int i = 0; i < Mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < Mapa.GetLength(1); j++)
+                {
+                    if (Mapa[i, j] == ' ')
+                    {
+                        esperado[i, j] = ' ';
+                    }
+                    if (Mapa[i, j] == 'A')
+                    {
+                        esperado[i, j] = 'A';
+                    }
+                    if (Mapa[i, j] == '*')
+                    {
+                        esperado[i, j] = '*';
+                        esperado[i, j - 1] = '*';
+                        esperado[i, j + 1] = '*';
+                        esperado[i - 1, j] = '*';
+                        esperado[i + 1, j] = '*';
+                    }
+                }
+            }
+            return esperado;
+        }
+    }
+}
---
+++ Dojo1/VS.Net/Dojo1/Dojo1/Testes.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dojo1
+{
+    [TestClass]
+    public class Testes
+    {
+        [TestMethod]
+        public void TestContagemAeroportos0()
+        {
+            char[,] mapa = new char[,] {
+            };
+
+            int resultado = Simulacao.ContarAeroportos(mapa);
+
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void TestContagemAeroportos1()
+        {
+            char[,] mapa = new char[,] {
+                {' ', ' ', ' '},
+                {' ', 'A', ' '},
+                {' ', ' ', ' '}
+            };
+
+            int resultado = Simulacao.ContarAeroportos(mapa);
+
+            Assert.AreEqual(1, resultado);
+        }
+
+        [TestMethod]
+        public void TestContagemAeroportos2()
+        {
+            char[,] mapa = new char[,] {
+                {' ', ' ', ' '},
+                {' ', 'A', 'A'},
+                {' ', ' ', ' '}
+            };
+
+            int resultado = Simulacao.ContarAeroportos(mapa);
+
+            Assert.AreEqual(2, resultado);
+        }
+
+        [TestMethod]
+        public void TestContagemAeroportos3()
+        {
+            char[,] mapa = new char[,] {
+                {' ', ' ', 'A'},
+                {' ', 'A', 'A'},
+                {' ', ' ', ' '}
+            };
+
+            int resultado = Simulacao.ContarAeroportos(mapa);
+
+            Assert.AreEqual(3, resultado);
+        }
+
+        [TestMethod]
+        public void TestContagemAeroportos4()
+        {
+            char[,] mapa = new char[,] {
+                {'A', 'A', 'A'},
+                {'A', 'A', 'A'},
+                {'A', 'A', 'A'}
+            };
+
+            int resultado = Simulacao.ContarAeroportos(mapa);
+
+            Assert.AreEqual(9, resultado);
+        }
+
+        [TestMethod]
+        public void TestExpansao0()
+        {
+            char[,] mapa = new char[,] {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+            };
+
+            char[,] esperado = new char[,] {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+            };
+
+            char[,] resultado = Simulacao.Expandir(mapa);
+
+            CollectionAssert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod]
+        public void TestExpansaoA()
+        {
+            char[,] mapa = new char[,] {
+                {' ', ' ', ' '},
+                {' ', 'A', ' '},
+                {' ', ' ', ' '}
+            };
+
+            char[,] esperado = new char[,] {
+                {' ', ' ', ' '},
+                {' ', 'A', ' '},
+                {' ', ' ', ' '}
+            };
+
+            char[,] resultado = Simulacao.Expandir(mapa);
+
+            CollectionAssert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod]
+        public void TestExpansao1()
+        {
+            char[,] mapa = new char[,] {
+                {' ', ' ', ' '},
+                {' ', '*', ' '},
+                {' ', ' ', ' '}
+            };
+
+            char[,] esperado = new char[,] {
+                {' ', '*', ' '},
+                {'*', '*', '*'},
+                {' ', '*', ' '}
+            };
+
+            char[,] resultado = Simulacao.Expandir(mapa);
+
+            CollectionAssert.AreEqual(esperado, resultado);
+        }
+    }
+}
